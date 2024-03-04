c1d417b35fbd79c67a304f51668dc590150994f9
egor <ega.nik5674@gmail.com>
2024-03-04 22:07:51 +0300

    Update shopapp/urls.py

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
 from shopapp.apps import ShopappConfig
-from shopapp.views import categories, get_good, index, get_category, add_product
+from shopapp.views import CategoryListView, ProductDetailView, ProductListView, ProductCategoryListView, ProductCreateView
 
 app_name = ShopappConfig.name
 
 urlpatterns = [
-    path('', index, name='main'),
-    path('categories/', categories, name='all_categories'),
-    path('goods/<int:pk>/', get_good, name='one_good'),
-    path('category/<int:pk>/', get_category, name='one_category'),
-    path('add/', add_product, name='add_product')
+    path('', ProductListView.as_view(), name='main'),
+    path('categories/', CategoryListView.as_view(), name='all_categories'),
+    path('goods/<int:pk>/', ProductDetailView.as_view(), name='one_good'),
+    path('category/<int:pk>/', ProductCategoryListView.as_view(), name='one_category'),
+    path('add/', ProductCreateView.as_view(), name='add_product')
 ]
 
