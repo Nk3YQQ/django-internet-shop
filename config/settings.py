e3193324058ebdd30da2b03819e4196867547947
egor <ega.nik5674@gmail.com>
2024-03-06 18:44:17 +0300

    Update config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shopapp',
+    'blogapp',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'skypro.test.testov@yandex.ru'
+EMAIL_HOST_PASSWORD = 'vqfqbjqhqvxskreu'
