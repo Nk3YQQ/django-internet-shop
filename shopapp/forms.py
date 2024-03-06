cd840977097e05a768726eea4364eee81bbf73ad
egor <ega.nik5674@gmail.com>
2024-03-06 18:43:51 +0300

    Update forms.py

--- shopapp/forms.py
+++ shopapp/forms.py
@@ -3,12 +3,12 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-    name = forms.CharField(label='Название', max_length=50, required=True)
-    content = forms.CharField(label='Описание', widget=forms.Textarea)
-    image = forms.ImageField(label='Изображение')
-    category = forms.ModelChoiceField(label='Категория', queryset=Category.objects.all(), required=True)
+    name = forms.CharField(label='Название', max_length=50)
+    content = forms.CharField(label='Описание', widget=forms.Textarea, required=False)
+    image = forms.ImageField(label='Изображение', required=False)
+    category = forms.ModelChoiceField(label='Категория', queryset=Category.objects.all())
     amount = forms.IntegerField(label='Цена')
 
     class Meta:
         model = Product
-        fields = ['name', 'content', 'category', 'image', 'amount']
+        fields = ('name', 'content', 'category', 'image', 'amount',)
