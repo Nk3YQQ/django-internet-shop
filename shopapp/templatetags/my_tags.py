17c86f121dbb804169ad234aef16346f155aec26
egor <ega.nik5674@gmail.com>
2024-03-21 16:19:16 +0300

    Update shopapp/templatetags/my_tags.py

--- shopapp/templatetags/my_tags.py
+++ shopapp/templatetags/my_tags.py
@@ -1,6 +1,5 @@
 from django import template
 
-
 register = template.Library()
 
 
@@ -9,3 +8,24 @@ def mymedia(val):
     if val:
         return f'/media/{val}'
     return '/media/shopapp/not_found.jpeg'
+
+
+@register.filter()
+def edit_or_create_button(obj):
+    if obj:
+        return 'Редактировать'
+    return 'Создать'
+
+
+@register.filter()
+def edit_or_create_good(obj):
+    if obj:
+        return 'Редактирование товара'
+    return 'Заполните поля для того, чтобы добавить товар'
+
+
+@register.filter()
+def check_version(obj):
+    if obj:
+        return f'Название версии: {obj}'
+    return 'Название версии: -'
