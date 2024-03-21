cbd2d66927e0f41a4bb28a7c29e3cc636c74432e
egor <ega.nik5674@gmail.com>
2024-03-21 16:17:32 +0300

    Update shopapp/forms.py

--- shopapp/forms.py
+++ shopapp/forms.py
@@ -1,14 +1,33 @@
 from django import forms
-from .models import Product, Category
+
+from config.settings import FORBIDDEN_WORDS
+from .models import Product, Version
+
+
+def check_words(clean_data, forbidden_words, verbose_name):
+    if clean_data.lower() in forbidden_words:
+        raise forms.ValidationError(f"В поле '{verbose_name}' обнаружено запрещённое слово")
 
 
 class ProductForm(forms.ModelForm):
-    name = forms.CharField(label='Название', max_length=50)
-    content = forms.CharField(label='Описание', widget=forms.Textarea, required=False)
-    image = forms.ImageField(label='Изображение', required=False)
-    category = forms.ModelChoiceField(label='Категория', queryset=Category.objects.all())
-    amount = forms.IntegerField(label='Цена')
+    def clean_name(self):
+        clean_data = self.cleaned_data.get('name')
+        verbose_name = self.fields.get('name').label
+        check_words(clean_data, FORBIDDEN_WORDS, verbose_name)
+        return clean_data
+
+    def clean_content(self):
+        clean_data = self.cleaned_data.get('content')
+        verbose_name = self.fields.get('content').label
+        check_words(clean_data, FORBIDDEN_WORDS, verbose_name)
+        return clean_data
 
     class Meta:
         model = Product
         fields = ('name', 'content', 'category', 'image', 'amount',)
+
+
+class VersionForm(forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
