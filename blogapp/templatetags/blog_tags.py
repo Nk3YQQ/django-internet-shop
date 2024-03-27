9c137d353059824ab0a534448c8f3b1e2c1b04fe
egor <ega.nik5674@gmail.com>
2024-03-27 18:12:15 +0300

    Update shopapp/blog_tags.py

--- blogapp/templatetags/blog_tags.py
+++ blogapp/templatetags/blog_tags.py
@@ -1,6 +1,5 @@
 from django import template
 
-
 register = template.Library()
 
 
@@ -9,3 +8,10 @@ def mymedia(val):
     if val:
         return f'/media/{val}'
     return '/media/shopapp/not_found.jpeg'
+
+
+@register.filter()
+def save_or_edit(val):
+    if val:
+        return 'Сохранить'
+    return 'Создать'
