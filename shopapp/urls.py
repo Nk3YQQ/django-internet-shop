0b4d4f05a8292d4ea74cac052eed5a28f9c31761
egor <ega.nik5674@gmail.com>
2024-04-08 15:54:12 +0300

    Update shopapp/urls.py

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from shopapp.apps import ShopappConfig
 from shopapp.views import (CategoryListView, ProductDetailView, ProductListView, ProductCategoryListView,
@@ -8,8 +9,8 @@ app_name = ShopappConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='main'),
-    path('categories/', CategoryListView.as_view(), name='all_categories'),
-    path('goods/<int:pk>/', ProductDetailView.as_view(), name='one_good'),
+    path('categories/', cache_page(60)(CategoryListView.as_view()), name='all_categories'),
+    path('goods/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='one_good'),
     path('category/<int:pk>/', ProductCategoryListView.as_view(), name='one_category'),
     path('add/', ProductCreateView.as_view(), name='add_product'),
     path('edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
