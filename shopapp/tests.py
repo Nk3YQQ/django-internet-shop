019e2c732f2570369e016c6674cbda5dc62a19e1
egor <ega.nik5674@gmail.com>
2024-05-02 14:27:06 +0300

    Update tests

--- shopapp/tests.py
+++ shopapp/tests.py
@@ -5,24 +5,19 @@ from shopapp.models import Product, Category
 
 class CategoryTestCase(TestCase):
     def setUp(self):
-        Category.objects.create(name='Ноутбуки')
-        Category.objects.create(name='Телефоны')
         Category.objects.create(name='Планшеты')
+        Category.objects.create(name='Телефоны')
 
     def test_category(self):
         categories_count = Category.objects.count()
-        self.assertEquals(categories_count, 3)
+        self.assertEquals(categories_count, 2)
 
 
 class ProductTestCase(TestCase):
     def setUp(self):
-        category_1 = Category.objects.get(name='Ноутбуки')
-        category_2 = Category.objects.get(name='Телефоны')
-        category_3 = Category.objects.get(name='Планшеты')
+        category = Category.objects.create(name='Ноутбуки')
 
-        Product.objects.create(name='Xiaomi Redmibook 15', category=category_1, amount=50000)
-        Product.objects.create(name='Xiaomi Redmi Note 8', category=category_2, amount=10000)
-        Product.objects.create(name='Apple iPad Pro', category=category_3, amount=100000)
+        Product.objects.create(name='Xiaomi Redmibook 15', category=category, amount=50000)
 
     def test_product(self):
         product_1 = Product.objects.get(name='Xiaomi Redmibook 15')
