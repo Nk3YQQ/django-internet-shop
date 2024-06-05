9e5ec38db79218b818fba87f34f7fd7870635d29
egor <ega.nik5674@gmail.com>
2024-06-05 21:58:59 +0300

    Update project

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Static
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = (
