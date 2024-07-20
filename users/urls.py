a7910a8de6be59e0a8bef9fc859aad07b0c049b3
egor <ega.nik5674@gmail.com>
2024-07-20 09:45:33 +0300

    Update users app

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,25 @@
 from django.urls import path
 
-from blogapp.views import UserMaterialListView
-from shopapp.views import UserProducts
+from blog.views import UserBlogListView
+from products.views import UserProducts
 from users.apps import UsersConfig
-from users.views import UserRegistration, LoginView, LogoutView, UserDetailView, UserUpdateView, reset_password
+from users.views import UserRegistration, LoginView, LogoutView, UserDetailView, UserUpdateView, reset_password, \
+    registration_done, ActivateAccountView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('register/', UserRegistration.as_view(), name='register'),
+    # Auth
+    path('registration/', UserRegistration.as_view(), name='register'),
+    path('registration/done/', registration_done, name='registration_done'),
+    path('registration/activate/<uuid:token>', ActivateAccountView.as_view(), name='registration_activate'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    # Users
     path('profile/', UserDetailView.as_view(), name='profile'),
-    path('edit/', UserUpdateView.as_view(), name='edit'),
-    path('respassword/', reset_password, name='respassword'),
+    path('edit/', UserUpdateView.as_view(), name='user_edit'),
+    path('reset/', reset_password, name='reset_password'),
     path('products/', UserProducts.as_view(), name='user_products'),
-    path('materials/', UserMaterialListView.as_view(), name='user_materials')
+    path('blog/', UserBlogListView.as_view(), name='user_blog')
 ]
